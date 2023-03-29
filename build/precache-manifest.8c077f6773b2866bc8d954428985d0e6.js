6329974961598da82584be5924ba1c80efb0cfd0
thuranaing <thuranaing@tamronnetwork.com>
2023-03-29 09:36:44 +0630

    update

---
+++ build/precache-manifest.8c077f6773b2866bc8d954428985d0e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c192861cf61c1e5a960",
+    "url": "/static/css/main.2a04dfbe.chunk.css"
+  },
+  {
+    "revision": "8c192861cf61c1e5a960",
+    "url": "/static/js/main.8c192861.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c6773d0644700904689aa74683e9eda6",
+    "url": "/index.html"
+  }
+];
